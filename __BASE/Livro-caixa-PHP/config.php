af9cd5bd1d36613e1509b620099ba7b2755363ce
Mateus Costa <costa.mack95@gmail.com>
2021-01-12 17:24:05 -0300

    Add proj base

---
+++ __BASE/Livro-caixa-PHP/config.php
@@ -0,0 +1,48 @@
+<?php
+
+//Configuracao do Banco de dados
+$host      = "localhost";
+$user      = "root";
+$pass      = "";
+$dbname    = "laravel_test";
+
+//Titulo do seu livro Caixa, geralmente seu nome
+$lc_titulo = "Seu nome";
+
+//Autenticacao simples
+$usuario   = "admin";
+$senha     = "123";
+
+//////////////////////////////////////
+//Nao altere a partir daqui!
+//////////////////////////////////////
+$conn      = mysqli_connect($host, $user, $pass, $dbname) or die("Erro na conexгo com a base de dados");
+$db        = mysqli_select_db($conn, $dbname) or die("Erro na seleзгo da base de dados");
+mysqli_set_charset( $conn, 'utf8' );
+
+if ( isset( $_SESSION['logado'] ) )
+{
+    $logado = $_SESSION['logado'];
+}
+else
+{
+    $logado = "";
+}
+
+$senha_    = md5($senha);
+
+if ( isset( $_POST['login'] ) && $_POST['login'] != '' )
+{
+    if ( $_POST['login'] == $usuario && $_POST['senha'] == $senha )
+    {
+        $logado = $_SESSION['logado'] = md5( $_POST['senha'] );
+        header( "Location: ./" );
+        exit();
+    }
+}
+
+if ( $logado != $senha_ && !isset( $pagina_login ) )
+{
+    header( "Location: login.php" );
+    exit();
+}
